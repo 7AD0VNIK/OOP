bbc2b46bff84fcc0da2aae78e1165157b85641da
Klim Sadov <k.sadov@g.nsu.ru>
2025-09-04 13:14:24 +0700

    codestyle refactor

--- Task_1_1_1/src/main/java/ru/nsu/k/sadov/Task_1_1_1/Sort.java
+++ Task_1_1_1/src/main/java/ru/nsu/k/sadov/Task_1_1_1/Sort.java
@@ -1,20 +1,30 @@
 package ru.nsu.k.sadov.Task_1_1_1;
 
+/**
+ * класс Sort содержит методы для реализации алгоритма пирамидальной сортировки
+ */
 public class Sort {
-    public static void heapify(int[] arr, int n, int i){
-        int lrgst = i;
-        int left = 2*i + 1;
-        int right = 2*i + 2;
+    /**
+     * функция для поддержания свойства максимальной кучи в поддереве с корнем i
+     * @param arr - массив, представляющий кучу
+     * @param n - размер кучи
+     * @param i - индекс корневого элемента поддерева
+     */
 
-        if (left < n && arr[lrgst] < arr[left] ) {
+    public static void heapify(int[] arr, int n, int i) {
+        int lrgst = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[lrgst] < arr[left]) {
             lrgst = left;
         }
 
-        if (right < n && arr[lrgst] < arr[right] ) {
+        if (right < n && arr[lrgst] < arr[right]) {
             lrgst = right;
         }
 
-        if (lrgst != i){
+        if (lrgst != i) {
             int swap = arr[i];
             arr[i] = arr[lrgst];
             arr[lrgst] = swap;
@@ -22,38 +32,59 @@ public class Sort {
             heapify(arr, n, lrgst);
         }
     }
+    /**
+     * функция для построения максимальной кучи из произвольного массива
+     * @param arr - массив для преобразования в кучу
+     * @param n - размер массива
+     */
 
-    public static void buildHeap(int[] arr, int n){
-        for (int i = n / 2 - 1; i >= 0; i--){
+    public static void buildHeap(int[] arr, int n) {
+        // начинаем с последнего нелистового узла (родителя последнего элемента)
+        // и движемся до корня
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
             heapify(arr, n, i);
         }
     }
-
-    public static void heapSort(int[] arr){
+    /**
+     * основная функция для выполнения пирамидальной сортировки
+     * @param arr - массив для сортировки
+     */
+    public static void heapSort(int[] arr) {
         int n = arr.length;
 
         buildHeap(arr, n);
 
 
-        for (int i = n - 1; i > 0; i--){
+        for (int i = n - 1; i > 0; i--) {
+            // двигаем корень в конец
             int tmp = arr[0];
             arr[0] = arr[i];
             arr[i] = tmp;
 
-            heapify(arr, i, 0);
+            heapify(arr, i, 0); // вызываем для уменьшенной кучи
         }
     }
-    public static void print(int[] arr){
+    /**
+     * вспомогательная функция для вывода массива в консоль
+     * @param arr - массив для вывода
+     */
+    public static void print(int[] arr) {
         System.out.print("[");
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
-            if (i < arr.length - 1){
+            if (i < arr.length - 1) {
                 System.out.print(", ");
             }
         }
         System.out.print("]");
     }
-    public static void main(String[] args){
+    /**
+     * основная функция для тестирования алгоритма сортировки
+     * @param args - аргументы командной строки
+     */
+
+    public static void main(String[] args) {
         int[] arrr = {4, 5, 3, 5, 2, 1, -1};
 
         heapSort(arrr);
