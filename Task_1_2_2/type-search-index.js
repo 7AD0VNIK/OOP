c476e0342aaa9b04d37761927ad95378ff1f7c95
7AD0VNIK <7AD0VNIK@users.noreply.github.com>
2025-11-01 08:00:00 +0000

    Deploying to gh-pages from @ 7AD0VNIK/OOP@4830fba8527b8b63c6dbcaea9608102596c408b3 🚀

--- Task_1_2_2/type-search-index.js
+++ Task_1_2_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.ksadov.HashhTable","l":"HashTable.Entry"},{"p":"ru.nsu.ksadov.HashhTable","l":"HashTable"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.ksadov.hashtable","l":"HashTable.Entry"},{"p":"ru.nsu.ksadov.hashtable","l":"HashTable"}];updateSearchResults();
