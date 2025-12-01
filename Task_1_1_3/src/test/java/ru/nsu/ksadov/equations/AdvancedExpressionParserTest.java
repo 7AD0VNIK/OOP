00fecb9446d968e9bd443bad9e468e3a9bbfe08b
Klim Sadov <k.sadov@g.nsu.ru>
2025-12-01 11:24:44 +0700

    -small refactor

--- Task_1_1_3/src/test/java/ru/nsu/ksadov/equations/AdvancedExpressionParserTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/ksadov/equations/AdvancedExpressionParserTest.java
@@ -9,25 +9,25 @@ class AdvancedExpressionParserTest {
     @Test
     void testParseFactor() {
         Expression e3 = AdvancedExpressionParser.parse("(1 + 2)");
-        assertEquals("(1.0+2.0)", e3.toString());
+        assertEquals("(1+2)", e3.toString());
     }
 
     @Test
     void testParseTerm() {
         Expression expr = AdvancedExpressionParser.parse("2 / 3");
-        assertEquals("(2.0/3.0)", expr.toString());
+        assertEquals("(2/3)", expr.toString());
     }
 
     @Test
     void testParseExpression() {
         Expression expr = AdvancedExpressionParser.parse("1 + 2 * 3");
-        assertEquals("(1.0+(2.0*3.0))", expr.toString());
+        assertEquals("(1+(2*3))", expr.toString());
     }
 
     @Test
     void testParseFull() {
         Expression expr = AdvancedExpressionParser.parse("(1 + 2) * 3");
-        assertEquals("((1.0+2.0)*3.0)", expr.toString());
+        assertEquals("((1+2)*3)", expr.toString());
     }
 
     @Test
