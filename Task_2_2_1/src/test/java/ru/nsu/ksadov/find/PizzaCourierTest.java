bf6e430123474be70f6a16799b9981e747e715e4
Klim Sadov <k.sadov@g.nsu.ru>
2026-03-02 19:57:59 +0700

    refactor

--- Task_2_2_1/src/test/java/ru/nsu/ksadov/find/PizzaCourierTest.java
+++ Task_2_2_1/src/test/java/ru/nsu/ksadov/find/PizzaCourierTest.java
@@ -1,6 +1,7 @@
 package ru.nsu.ksadov.find;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
@@ -21,6 +22,22 @@ class PizzaCourierTest {
 
         courier.stop();
         courierThread.interrupt();
+    }
+
+    @Test
+    void testCourierStopsWhenStorageEmpty() throws InterruptedException {
+        PizzaBuffer<Order> storage = new PizzaBuffer<>(5); // empty storage
+        PizzaCourier courier = new PizzaCourier(2, 3, 100, storage);
+        Thread courierThread = new Thread(courier);
+        courierThread.start();
+
+        Thread.sleep(50);
+
+        courier.stop();
+        courierThread.interrupt();
+        courierThread.join(500);
+
+        assertFalse(courierThread.isAlive(), "Courier thread should have terminated");
     }
 
 }
