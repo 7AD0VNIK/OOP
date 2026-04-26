2ece69070218909a5abe8b983450f1d22053c0be
Klim Sadov <k.sadov@g.nsu.ru>
2026-04-26 15:51:10 +0700

    refactor

--- Task_2_1_2/src/main/java/ru/nsu/ksadov/find/Announcer.java
+++ Task_2_1_2/src/main/java/ru/nsu/ksadov/find/Announcer.java
@@ -9,43 +9,43 @@ import java.net.InetAddress;
  * Implements announce of connection.
  */
 public class Announcer implements Runnable {
-        private final int tcpPort;
-        private final String mulGroup = "224.0.0.1";
-        private final int mulPort = 8888;
-        private final String magicN = "server prime port here:";
+    private final int tcpPort;
+    private final String mulGroup = "224.0.0.1";
+    private final int mulPort = 8888;
+    private final String magicN = "server prime port here:";
 
-        /**
-         * Instantiates a new Announcer.
-         */
-        public Announcer(int tcpPort) {
-            this.tcpPort = tcpPort;
-        }
+    /**
+     * Instantiates a new Announcer.
+     */
+    public Announcer(int tcpPort) {
+        this.tcpPort = tcpPort;
+    }
 
-        /**
-         * Runs the announcer loop to broadcast server presence.
-         */
-        @Override
-        public void run() {
-            try (DatagramSocket socket = new DatagramSocket()) {
-                InetAddress group = InetAddress.getByName(mulGroup);
-                String msg = magicN + tcpPort;
-                byte[] buf = msg.getBytes();
+    /**
+     * Runs the announcer loop to broadcast server presence.
+     */
+    @Override
+    public void run() {
+        try (DatagramSocket socket = new DatagramSocket()) {
+            InetAddress group = InetAddress.getByName(mulGroup);
+            String msg = magicN + tcpPort;
+            byte[] buf = msg.getBytes();
 
-                System.out.println("MC started...");
+            System.out.println("MC started...");
 
-                while (!Thread.currentThread().isInterrupted()) {
-                    DatagramPacket packet = new DatagramPacket(buf,
-                            buf.length, group, mulPort);
-                    socket.send(packet);
+            while (!Thread.currentThread().isInterrupted()) {
+                DatagramPacket packet = new DatagramPacket(buf,
+                        buf.length, group, mulPort);
+                socket.send(packet);
 
-                    try {
-                        Thread.sleep(1500);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    }
+                try {
+                    Thread.sleep(1500);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
                 }
-            } catch (IOException e) {
-                System.out.println("MC announcer error" + e.getMessage());
             }
+        } catch (IOException e) {
+            System.out.println("MC announcer error" + e.getMessage());
         }
+    }
 }
